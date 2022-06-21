b650e6972c795372e7665d30e124b6b87547ca95
indestinee <indestinee@gmail.com>
2022-06-21 22:17:05 +0800

    add draft and select function (#2)

--- src/redux/sudokuSlice.ts
+++ src/redux/sudokuSlice.ts
@@ -4,35 +4,53 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 export interface SudokuSquareState {
   index: number,
   value: number,
-  draft: number[],
-}
-
-export interface SudokuUpdateParam {
-  index: number,
-  value: SudokuSquareState,
+  draft: boolean[],
 }
 
 interface SudokuState {
   squares: SudokuSquareState[],
+  selected: number,
+  count: number[],
+}
+
+export interface UpdateCountParam {
+  index: number,
+  value: number,
 }
 
 const initialState: SudokuState = {
-  squares: Array(81).map((_, index:number) => ({
+  squares: [...Array(81)].map((_, index:number) => ({
     index,
-    value: 0,
-    draft: [],
+    value: -1,
+    draft: [...Array(9)].map(() => false),
   })),
+  selected: -1,
+  count: [...Array(9)].map(() => 0),
 };
 
 export const sudokuSlice = createSlice({
   name: 'sudoku',
   initialState,
   reducers: {
-    setSquares: (state, action: PayloadAction<SudokuUpdateParam>) => {
-      state.squares[action.payload.index] = action.payload.value;
+    setSquare: (state, action: PayloadAction<SudokuSquareState>) => {
+      state.squares[action.payload.index] = action.payload;
+    },
+    setSelected: (state, action: PayloadAction<number>) => {
+      state.selected = action.payload;
+    },
+    setCount: (state, action: PayloadAction<UpdateCountParam>) => {
+      state.count[action.payload.index] = state.count[action.payload.value];
+    },
+    deltaCount: (state, action: PayloadAction<UpdateCountParam>) => {
+      state.count[action.payload.index] += action.payload.value;
     },
   },
 });
 
-export const {setSquares} = sudokuSlice.actions;
+export const {
+  setSquare,
+  setSelected,
+  setCount,
+  deltaCount,
+} = sudokuSlice.actions;
 export default sudokuSlice.reducer;
