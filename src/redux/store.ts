6bb1a1c4e2944b90d916b19ec998d231f48c311e
indestinee <indestinee@gmail.com>
2022-06-23 20:32:12 +0800

    Revert "dev ui (#3)"
    This reverts commit a7eae8d830af62b4286edb34248385309a5690b2.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,8 @@
 import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import sudokuSlice from './sudokuSlice';
 
 export const store = configureStore({
   reducer: {
-    sudoku: sudokuSlice,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type AppDispatch = typeof store.dispatch;
